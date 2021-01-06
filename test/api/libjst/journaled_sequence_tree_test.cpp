932f27271c7cb8dea91c26bee404f38be964ba8d
rrahn <rene.rahn@fu-berlin.de>
2021-01-06 10:48:55 +0100

    [INFRA] Initial test infrastructure for jstmap.

---
+++ test/api/libjst/journaled_sequence_tree_test.cpp
@@ -0,0 +1,25 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <libjst/journaled_sequence_tree.hpp>
+
+TEST(journaled_sequence_tree, create)
+{
+    // TODO write me!
+}
+
+TEST(journaled_sequence_tree, save)
+{
+    // TODO write me!
+}
+
+TEST(journaled_sequence_tree, load)
+{
+    // TODO write me!
+}
