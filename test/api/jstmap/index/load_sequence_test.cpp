e5904939f66de4b92d5c7b5930c6d53d3c782778
rrahn <rene.rahn@fu-berlin.de>
2021-01-07 19:37:23 +0100

    [TEST] Add test for loading the sequences.

--- test/api/jstmap/index/load_sequence_test.cpp
+++ test/api/jstmap/index/load_sequence_test.cpp
@@ -7,9 +7,26 @@
 
 #include <gtest/gtest.h>
 
+// Including required debug stream types for range_eq test.
+#include <seqan3/core/detail/debug_stream_alphabet.hpp>
+#include <seqan3/core/detail/debug_stream_range.hpp>
+#include <seqan3/test/expect_range_eq.hpp>
+
 #include <jstmap/index/load_sequence.hpp>
+
+#include "test_data.hpp"
 
 TEST(jstmap_index, load_sequence)
 {
-    //TODO: write me!
+    using seqan3::operator""_dna5;
+
+    std::filesystem::path input_file{DATADIR"in.fasta"};
+    std::vector actual_sequences = jstmap::load_sequences(input_file);
+
+    std::vector expected_sequences{
+        "ACGTTTGATTCGCG"_dna5,
+        "TCGGGGGATTCGCG"_dna5
+    };
+
+    EXPECT_RANGE_EQ(actual_sequences, expected_sequences);
 }
---
+++ test/api/jstmap/index/test_data.hpp
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <jstmap/index/global_types.hpp>
+
+namespace jstmap::test
+{
+
+using seqan3::operator""_dna5;
+
+inline const raw_sequence_t reference = "aacctt"_dna5;
+
+inline const std::vector<raw_sequence_t> sequences
+{
+    "aaaaaa"_dna5,
+    "cccccc"_dna5,
+    "tttttt"_dna5
+};
+
+}  // namespace jstmap::test
