c450b432033568d865027b2878a4eb46baf99c34
rrahn <rene.rahn@fu-berlin.de>
2021-03-31 16:41:28 +0200

    [INFRA] Add benchmark infrastructure to the jstmap.
    Also adds a simple test benchmark.

--- test/jstmap_test.cmake
+++ test/jstmap_test.cmake
@@ -30,12 +30,14 @@ list(APPEND CMAKE_MODULE_PATH "${SEQAN3_TEST_CMAKE_MODULE_DIR}")
 # The trick is here to provide a cmake file as a directory property that executes the build command.
 file (WRITE "${CMAKE_CURRENT_BINARY_DIR}/build_test_targets.cmake"
             "execute_process(COMMAND ${CMAKE_COMMAND} --build . --target api_test)\n"
-            "execute_process(COMMAND ${CMAKE_COMMAND} --build . --target cli_test)")
+            "execute_process(COMMAND ${CMAKE_COMMAND} --build . --target cli_test)\n"
+            "execute_process(COMMAND ${CMAKE_COMMAND} --build . --target benchmark_test)")
 set_directory_properties (PROPERTIES TEST_INCLUDE_FILE "${CMAKE_CURRENT_BINARY_DIR}/build_test_targets.cmake")
 
 # Define the test targets. All depending targets are built just before the test execution.
 add_custom_target (api_test)
 add_custom_target (cli_test)
+add_custom_target (benchmark_test)
 
 # Test executables and libraries should not mix with the application files.
 unset (CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
@@ -46,16 +48,26 @@ unset (CMAKE_RUNTIME_OUTPUT_DIRECTORY)
 find_path (SEQAN3_TEST_INCLUDE_DIR NAMES seqan3/test/expect_range_eq.hpp
                                    HINTS "${CMAKE_SOURCE_DIR}/lib/seqan3/test/include/")
 
+#--------------------------------------------------------------------
+# Cmake interface targets
+#--------------------------------------------------------------------
+
 add_library (jstmap_test INTERFACE)
 target_include_directories (jstmap_test INTERFACE "${SEQAN3_TEST_INCLUDE_DIR}")
 target_compile_options (jstmap_test INTERFACE "-pedantic"  "-Wall" "-Wextra" "-Werror")
-target_link_libraries (jstmap_test INTERFACE "pthread")
+target_compile_features (jstmap_test INTERFACE cxx_std_20)
+target_link_libraries (jstmap_test INTERFACE "pthread" "seqan3::seqan3")
 add_library (jstmap::test ALIAS jstmap_test)
 
 add_library (jstmap_test_unit INTERFACE)
 target_include_directories (jstmap_test_unit INTERFACE "gtest" "gtest_main" "jstmap::test")
 target_link_libraries (jstmap_test_unit INTERFACE "gtest" "gtest_main" "jstmap::test")
 add_library (jstmap::test::unit ALIAS jstmap_test_unit)
+
+add_library (jstmap_test_performance INTERFACE)
+target_include_directories (jstmap_test_performance INTERFACE "gbenchmark" "jstmap::test")
+target_link_libraries (jstmap_test_performance INTERFACE "gbenchmark" "jstmap::test" )
+add_library (jstmap::test::performance ALIAS jstmap_test_performance)
 
 # ----------------------------------------------------------------------------
 # Commonly used macros for the different test modules.
@@ -64,7 +76,9 @@ add_library (jstmap::test::unit ALIAS jstmap_test_unit)
 include (app_datasources)
 include (${CMAKE_CURRENT_LIST_DIR}/data/datasources.cmake)
 
+include (seqan3_require_benchmark)
 include (seqan3_require_test)
 include (add_subdirectories)
+include (seqan3_test_component)
 
 message (STATUS "${FontBold}You can run `make test` to build and run tests.${FontReset}")
