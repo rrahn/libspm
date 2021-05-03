ee35db620e9a5a3c9992eec685babb4353a8d829
rrahn <rahn@molgen.mpg.de>
2021-05-03 19:03:56 +0200

    [FEATURE] Add initial view sub-command to jstmap.
    This sub-command allows to visualise any particular haplotype in fasta format.

---
+++ jstmap-view/jstmap/view/load_jst.hpp
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/rrahn/just_map/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <filesystem>
+
+#include <jstmap/view/global_types.hpp>
+
+namespace jstmap
+{
+
+jst_t load_jst(std::filesystem::path const &);
+
+} // namespace jstmap
