52b84c33aafd0ec7f4d4c1f07c1f5aaf50ca70bf
rrahn <rahn@molgen.mpg.de>
2021-06-14 15:20:17 +0200

    [TEST] Add pigeonhole filter benchmark.
    An initial benchmark to analyse the pigeonhole performance on a jst.
    
    Some results with AVX512 single threaded:
    
    ```
    Run on (80 X 3900 MHz CPU s)
    CPU Caches:
      L1 Data 32K (x40)
      L1 Instruction 32K (x40)
      L2 Unified 1024K (x40)
      L3 Unified 28160K (x2)
    Load Average: 4.26, 4.34, 4.16
    -------------------------------------------------------------------------------------------------------------
    Benchmark                                                   Time             CPU   Iterations UserCounters...
    -------------------------------------------------------------------------------------------------------------
    pigeonhole_filter_bench/pigeonhole_filter/50/100   8936379053 ns   8933938813 ns            1 #hits=73.9055M bytes_per_second=27.183M/s
    pigeonhole_filter_bench/pigeonhole_filter/50/150   10377487313 ns   10375200062 ns            1 #hits=73.9219M bytes_per_second=23.4069M/s
    pigeonhole_filter_bench/pigeonhole_filter/50/200   11975985970 ns   11973129571 ns            1 #hits=73.9226M bytes_per_second=20.283M/s
    pigeonhole_filter_bench/pigeonhole_filter/50/250   9335872795 ns   9333949014 ns            1 #hits=55.4747M bytes_per_second=26.018M/s
    pigeonhole_filter_bench/pigeonhole_filter/50/300   10220037572 ns   10217896146 ns            1 #hits=55.4706M bytes_per_second=23.7672M/s
    pigeonhole_filter_bench/pigeonhole_filter/100/100  8786682952 ns   8785009236 ns            1 #hits=129.339M bytes_per_second=27.6438M/s
    pigeonhole_filter_bench/pigeonhole_filter/100/150  10604581095 ns   10602652716 ns            1 #hits=129.374M bytes_per_second=22.9047M/s
    pigeonhole_filter_bench/pigeonhole_filter/100/200  11830749162 ns   11828317640 ns            1 #hits=147.847M bytes_per_second=20.5313M/s
    pigeonhole_filter_bench/pigeonhole_filter/100/250  9501356568 ns   9499297159 ns            1 #hits=129.395M bytes_per_second=25.5651M/s
    pigeonhole_filter_bench/pigeonhole_filter/100/300  10643365063 ns   10641179798 ns            1 #hits=129.455M bytes_per_second=22.8218M/s
    pigeonhole_filter_bench/pigeonhole_filter/500/100  10485912360 ns   10483422765 ns            1 #hits=683.687M bytes_per_second=23.1652M/s
    pigeonhole_filter_bench/pigeonhole_filter/500/150  12331844144 ns   12328791942 ns            1 #hits=665.262M bytes_per_second=19.6979M/s
    pigeonhole_filter_bench/pigeonhole_filter/500/200  9930551872 ns   9928621576 ns            1 #hits=683.879M bytes_per_second=24.4597M/s
    pigeonhole_filter_bench/pigeonhole_filter/500/250  11436725590 ns   11434377850 ns            1 #hits=665.403M bytes_per_second=21.2387M/s
    11436725590pigeonhole_filter_bench/pigeonhole_filter/500/300  12567125347 ns   12564563877 ns            1 #hits=683.99M bytes_per_second=19.3282M/s
    pigeonhole_filter_bench/pigeonhole_filter/1000/100 10544256292 ns   10541994226 ns            1 #hits=1.40433G bytes_per_second=23.0365M/s
    pigeonhole_filter_bench/pigeonhole_filter/1000/150 12699915010 ns   12696332247 ns            1 #hits=1.34905G bytes_per_second=19.1276M/s
    pigeonhole_filter_bench/pigeonhole_filter/1000/200 10194827676 ns   10192606592 ns            1 #hits=1.34932G bytes_per_second=23.8262M/s
    pigeonhole_filter_bench/pigeonhole_filter/1000/250 11810111936 ns   11807473451 ns            1 #hits=1.34939G bytes_per_second=20.5676M/s
    pigeonhole_filter_bench/pigeonhole_filter/1000/300 13012146465 ns   13009275415 ns            1 #hits=1.38654G bytes_per_second=18.6675M/s
    ```

---
+++ test/benchmark/jstmap/pigeonhole_filter_benchmark.cpp
@@ -0,0 +1,78 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <benchmark/benchmark.h>
+
+#include <libjst/search/pigeonhole_filter.hpp>
+#include <libjst/search/state_manager_stack.hpp>
+
+#include <jstmap/search/load_queries.hpp>
+#include <jstmap/search/search_queries.hpp>
+
+#include "benchmark_utility.hpp"
+
+template <typename jst_t>
+auto sample_reads(jst_t const & jst, size_t const read_count, size_t const read_size)
+{
+    auto enumerator = jst.context_enumerator(read_size);
+    size_t const sample_rate = std::ceil(jst.reference().size() / read_count);
+    size_t counter = 0;
+    seqan::StringSet<sequence_t> sampled_reads{};
+
+    for (auto it = enumerator.begin(); it != enumerator.end(); ++it)
+    {
+        if (++counter % sample_rate == 0)
+        {
+            auto read = *it;
+            seqan::appendValue(sampled_reads, sequence_t{read.begin(), read.end()});
+        }
+    }
+    return sampled_reads;
+}
+
+template <typename ...args_t>
+static void pigeonhole_filter_bench(benchmark::State & state, args_t && ...args)
+{
+    auto [jst_file] = std::tuple{args...};
+    size_t const read_count = state.range(0);
+    size_t const read_length = state.range(1);
+    auto jst = jstmap::load_jst(jst_file);
+    auto sampled_reads = sample_reads(jst, read_count, read_length);
+
+    // Prepare pigeonhole filter.
+    using state_t = typename decltype(libjst::pigeonhole_filter{sampled_reads})::state_type;
+    libjst::pigeonhole_filter filter{sampled_reads, 0.0, libjst::search_state_manager_stack<state_t>{}};
+
+    size_t const fragment_size = filter.qgram_size();
+
+    size_t hit_count{};
+    for (auto _ : state)
+    {
+        hit_count = 0;
+        auto jst_range_agent = jst.range_agent(fragment_size, filter.state_manager()); // already pushing a branch.
+        filter(jst_range_agent, [&] (auto const &, auto const &) { ++hit_count; });
+    }
+
+    state.counters["bytes_per_second"] = seqan3::test::bytes_per_second(jst.total_symbol_count());
+    state.counters["#hits"] = hit_count;
+}
+
+static void CustomArguments(benchmark::internal::Benchmark* b)
+{
+    std::array range1{50, 100, 500, 1000};
+    std::array range2{100, 150, 200, 250, 300};
+
+    for (size_t i = 0; i < range1.size(); ++i)
+        for (size_t j = 0; j < range2.size(); ++j)
+            b->Args({range1[i], range2[j]});
+}
+
+BENCHMARK_CAPTURE(pigeonhole_filter_bench,
+                  pigeonhole_filter,
+                  DATADIR"sim_ref_10Kb_SNP_INDELs")->Apply(CustomArguments);
+
+BENCHMARK_MAIN();
