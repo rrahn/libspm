e85e53867374d802d8d927ae2aae7dd9613e72c8
rrahn <rahn@molgen.mpg.de>
2021-06-23 09:14:18 +0200

    [FEEATURE] Adds a reference postition type to access the underlying reference collcetion.
    Adapts the traverser model to store internally the new reference position type.
    The accessor functions still represent a single reference.

---
+++ libjst/libjst/reference_position.hpp
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides libjst::reference_position.
+ * \author Rene Rahn <rene.rahn AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <concepts>
+
+namespace libjst
+{
+/*!\brief A specific position type for the libjst::journaled_sequence_tree reference collecetion.
+ *
+ * \details
+ *
+ * This position can be used to index any position within the underlying reference collection.
+ * The index identifies the reference within the set and the offset gives the position whithin this
+ * reference sequence.
+ */
+struct reference_position
+{
+    size_t idx; //!< The index of the reference sequence.
+    size_t offset; //!< The offset within the respective reference sequence.
+
+    //!\brief Default three-way comparison.
+    constexpr auto operator<=>(reference_position const &) const noexcept = default;
+};
+
+/*!\brief A specific position type for the libjst::journaled_sequence_tree reference collection.
+ * \relates libjst::reference_position
+ *
+ * \tparam stream_t The type of the output stream.
+ * \tparam reference_position_t The type of the reference postion; must be the same as libjst::reference_position.
+ *
+ * \param[in] stream The stream to write to.
+ * \param[in] reference_pos The object to stream.
+ */
+template <typename stream_t, typename reference_position_t>
+//!\cond
+    requires std::same_as<std::remove_cvref_t<reference_position_t>, reference_position>
+//!\endcond
+stream_t & operator<<(stream_t & stream, reference_position && reference_pos)
+{
+    stream << "["
+                << "idx: " << reference_pos.idx << ", "
+                << "pos: " << reference_pos.offset
+           << "]";
+    return stream;
+}
+}  // namespace libjst
