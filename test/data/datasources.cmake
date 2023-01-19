3703645388976190cfa6b2789bccfc8937b343a2
rrahn <rene.rahn@fu-berlin.de>
2023-01-19 11:11:21 +0100

    [INFRA] Add current jst test data set to datasource.
    It is now loaded from the public ftp server ftp.imp.fu-berlin.de

--- test/data/datasources.cmake
+++ test/data/datasources.cmake
@@ -109,3 +109,11 @@ declare_datasource (FILE sim_ref_10Kb_no_variants.vcf
 
 declare_datasource (FILE Ash1_v2.2.fa.gz
                     URL ftp://ftp.ccb.jhu.edu/pub/data/Homo_sapiens/Ash1/v2.2/Assembly/Ash1_v2.2.fa.gz)
+
+declare_datasource (FILE ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.jst
+                    URL ftp://ftp.imp.fu-berlin.de/pub/rmaerker/jstmap/current/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.jst
+                    URL_HASH SHA256=9f787f1f0b14375a9da695d42457da13a7cf8a4ee977ff5b0d1867717b2acd17)
+
+declare_datasource (FILE ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.jst.ibf
+                    URL ftp://ftp.imp.fu-berlin.de//pub/rmaerker/jstmap/current/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.jst.ibf
+                    URL_HASH SHA256=c1cc172b447bb0b4d95b62a1b948e5c23e8bb82b743c38aba0fac071b6399a33)
