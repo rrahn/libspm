85d42e5769141b960e606b8ba4f1dda0383d0f4a
rrahn <rene.rahn@fu-berlin.de>
2023-01-19 11:15:00 +0100

    [APP] Fix read sampler to only sample from non-N-repeats.
    Read sampler now samples only reads from the reference without the leading and trailing N regions of a reference contig.

--- jstmap-simulate/jstmap/simulate/read_sampler.cpp
+++ jstmap-simulate/jstmap/simulate/read_sampler.cpp
@@ -23,6 +23,7 @@
 #include <libjst/sequence_tree/volatile_tree.hpp>
 
 #include <jstmap/simulate/read_sampler.hpp>
+#include <jstmap/global/application_logger.hpp>
 
 namespace jstmap
 {
@@ -32,6 +33,8 @@ namespace jstmap
 
     libjst::tree_stats
     read_sampler::compute_tree_stats(size_t const size) const {
+        log_debug("Compute tree statistics");
+        log_debug("Variant count:", _rcs_store.variants().size());
         return libjst::volatile_tree(_rcs_store) | libjst::labelled<libjst::sequence_label_kind::root_path>()
                                                  | libjst::coloured()
                                                  | libjst::trim(size - 1)
@@ -41,17 +44,20 @@ namespace jstmap
     }
 
     sampled_read_list_type
-    read_sampler::sample_reads(libjst::tree_stats const stats,  size_t const count,  size_t const size) const {
+    read_sampler::sample_reads(libjst::tree_stats const & stats,  size_t const count,  size_t const size) const {
         assert(size > 0);
         assert(count > 0);
 
         std::mt19937 random_engine{42};
         size_t const window_size = size - 1;
-        size_t max_sample_size = stats.symbol_count;
-        std::vector<size_t> sample_hints = generate_sample_hints(count, max_sample_size, random_engine);
+        auto sample_range = compute_sample_range(stats, size);
+
+        log_debug("Window size:", window_size);
+        log_debug("Max sample range", sample_range);
+        std::vector<size_t> sample_positions = generate_sample_positions(count, sample_range, random_engine);
 
         sampled_read_list_type sampled_reads{};
-        sampled_reads.reserve(sample_hints.size());
+        sampled_reads.reserve(sample_positions.size());
 
         auto sample_tree = libjst::volatile_tree(_rcs_store) | libjst::labelled<libjst::sequence_label_kind::root_path>()
                                                              | libjst::coloured()
@@ -62,41 +68,57 @@ namespace jstmap
                                                              | libjst::seek();
 
         libjst::tree_traverser_base path{sample_tree};
-        auto hint_it = sample_hints.begin();
-        size_t last_hint{0};
-        std::ptrdiff_t remaining = *hint_it;
+        auto next_sample_position = sample_positions.begin();
         auto tree_it = path.begin();
+        size_t last_position = window_size;
         while (tree_it != path.end()) {
             auto cargo = *tree_it;
             auto label = cargo.sequence();
-            remaining -= std::max<std::ptrdiff_t>(std::ranges::ssize(label) - window_size, 0);
-            if (remaining <= 0) { // found label to sample from
-                std::ptrdiff_t label_offset = (std::ranges::ssize(label) + remaining) - size;
-                assert(label_offset >= 0);
-                auto first = std::ranges::next(std::ranges::begin(label), label_offset, std::ranges::end(label));
+            assert(std::ranges::size(label) >= window_size);
+            size_t current_position = last_position + std::ranges::size(label) - window_size;
+            if (*next_sample_position <= current_position) { // found sample position
+                log_debug("====== Found sample position ======");
+                log_debug("Current position:", current_position);
+                log_debug("Sample position:", *next_sample_position);
+                log_debug("Label size extended:", std::ranges::size(label));
+                log_debug("Label size normal:", std::ranges::size(label) - window_size);
+                log_debug("Sample id:", std::ranges::distance(sample_positions.begin(), next_sample_position));
 
-                sampled_reads.emplace_back(read_type{first, std::ranges::next(first, size, std::ranges::end(label))},
+                std::ptrdiff_t sample_end_offset = current_position - *next_sample_position;
+                std::ptrdiff_t sample_end = std::ranges::size(label) - sample_end_offset;
+                std::ptrdiff_t sample_begin = sample_end - size;
+
+                log_debug("Sample end offset:", sample_end_offset);
+                log_debug("Sample end:", sample_end);
+                log_debug("Sample begin:", sample_begin);
+
+                assert(sample_end_offset < (std::ranges::size(label) - window_size));
+                assert(sample_end >= size);
+
+                auto read_begin = std::ranges::next(std::ranges::begin(label), sample_begin);
+                auto read_end = std::ranges::next(std::ranges::begin(label), sample_end);
+                sampled_reads.emplace_back(read_type{read_begin, read_end},
                                            cargo.position(),
-                                           label_offset);
-                assert(std::ranges::size(get<0>(sampled_reads.back())) == size);
+                                           sample_end_offset);
+                validate_sample(sampled_reads.back());
+                // assert();
 
-                last_hint = *hint_it;
-                if (++hint_it == sample_hints.end())
+                if (++next_sample_position == sample_positions.end())
                     break;
-
-                remaining = (*hint_it - last_hint) + (label_offset + size); // TODO: fix me!
             } else {
                 ++tree_it;
+                last_position = current_position;
             }
         }
         return sampled_reads;
     }
 
-    std::vector<size_t> read_sampler::generate_sample_hints(size_t const count,
-                                                            size_t const max_sample_size,
-                                                            std::mt19937 & random_engine) const {
+    std::vector<size_t> read_sampler::generate_sample_positions(size_t const count,
+                                                                std::pair<size_t, size_t> const max_sample_range,
+                                                                std::mt19937 & random_engine) const {
 
-        std::uniform_int_distribution<size_t> sample_dist{0, max_sample_size};
+        auto [from, to] = max_sample_range;
+        std::uniform_int_distribution<size_t> sample_dist{from, to};
         std::vector<size_t> tmp{};
         tmp.resize(count);
         std::ranges::generate(tmp, [&] () { return sample_dist(random_engine); });
@@ -104,26 +126,50 @@ namespace jstmap
         return tmp;
     }
 
-    // read_type
-    // read_sampler::sample_read(libjst::seek_position const pos,
-    //                           std::ptrdiff_t const label_offset,
-    //                           size_t const read_size) const {
+    std::pair<size_t, size_t> read_sampler::compute_sample_range(libjst::tree_stats const & stats,
+                                                                 size_t const sample_size) const noexcept {
+        auto const & reference = _rcs_store.source();
+        auto first_it = std::ranges::find_if_not(reference, [] (auto const nucleotide) {
+            return seqan3::to_char(nucleotide) == 'N';
+        });
 
-    //     constexpr auto is_N = [] (auto const & symbol) constexpr {
-    //         return symbol == seqan3::from_char(alphabet_t{}, 'N');
-    //     };
+        auto last_it = std::ranges::find_if_not(reference | std::views::reverse, [] (auto const nucleotide) {
+            return seqan3::to_char(nucleotide) == 'N';
+        }).base();
 
-    //     std::ptrdiff_t end_offset = std::ranges::ssize(label) + remaining;
-    //     std::ptrdiff_t start_offset = end_offset - size;
-    //     assert(end_offset >= size);
-    //     assert(start_offset >= 0);
-    //     auto first = std::ranges::next(std::ranges::begin(label), start_offset, std::ranges::end(label));
+        size_t left_overhead = std::ranges::distance(std::ranges::begin(reference), first_it);
+        size_t right_overhead = std::ranges::distance(last_it, std::ranges::end(reference));
+        log_debug("Max subtree depth:", stats.max_subtree_depth);
+        log_debug("Left overhead:", left_overhead);
+        log_debug("Right overhead:", right_overhead);
+        log_debug("Symbol count:", stats.symbol_count);
+        assert(stats.symbol_count > (left_overhead + right_overhead + sample_size));
 
-    //     sampled_reads.emplace_back();
-    //     assert(std::ranges::size(sampled_reads.back()) == size);
+        return std::pair{left_overhead + sample_size, stats.symbol_count - right_overhead};
+    }
 
-    //     read_type read{first, std::ranges::next(first, size, std::ranges::end(label))}
+    bool read_sampler::validate_sample(sampled_read_type const & sample) const noexcept {
+        auto && [read, seek_pos, offset] = sample;
+        size_t sample_size = std::ranges::size(read);
+        size_t window_size = sample_size - 1;
+        auto validation_tree = libjst::volatile_tree(_rcs_store) | libjst::labelled<libjst::sequence_label_kind::root_path>()
+                                                                 | libjst::coloured()
+                                                                 | libjst::trim(window_size)
+                                                                 | libjst::prune()
+                                                                 | libjst::left_extend(window_size)
+                                                                 | libjst::merge()
+                                                                 | libjst::seek();
 
-    //     return sampled_reads;
-    // }
+        auto node = validation_tree.seek(seek_pos);
+        auto cargo = *node;
+        auto label = cargo.sequence();
+        size_t end_position = std::ranges::size(label) - offset;
+        size_t begin_position = end_position - sample_size;
+        auto label_begin = std::ranges::next(std::ranges::begin(label), begin_position);
+        auto label_end = std::ranges::next(std::ranges::begin(label), end_position);
+        bool test = std::ranges::equal(read, std::ranges::subrange{label_begin, label_end});
+        if (!test)
+            log_warn("The sampled read does not corresponds with the sequence at the given seek position");
+        return test;
+    }
 } // namespace jstmap
