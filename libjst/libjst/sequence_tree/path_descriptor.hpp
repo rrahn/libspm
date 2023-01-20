fc973851f8575e4a1e1512ab96e491bec704d9cf
rrahn <rene.rahn@fu-berlin.de>
2023-01-20 16:40:13 +0100

    [LIB] Add larger paths in path descriptor.
    Adds extended word using 4x64 bit words to handle alternate paths with a maximum depth of 256.

--- libjst/libjst/sequence_tree/path_descriptor.hpp
+++ libjst/libjst/sequence_tree/path_descriptor.hpp
@@ -12,21 +12,114 @@
 
 #pragma once
 
+#include <array>
 #include <bit>
 #include <bitset>
 #include <cstdint>
+#include <iosfwd>
 #include <iterator>
+
+#include <cereal/types/array.hpp>
 
 #include <seqan3/core/concept/cereal.hpp>
 
 namespace libjst
 {
 
+    template <typename word_t = uint_fast64_t>
+    class extended_word {
+    private:
+
+        static constexpr size_t word_count = 4;
+        static constexpr size_t bits_per_word = sizeof(word_t) << 3;
+        static constexpr word_t carry_msb_mask = static_cast<word_t>(1) << (bits_per_word - 1);
+
+        template <size_t idx, typename value_t>
+        struct indexed_word {
+            value_t value;
+
+            static constexpr size_t offset = bits_per_word * idx;
+
+            constexpr size_t bit_width() const noexcept {
+                return std::bit_width(value) + (value != 0) * offset;
+            }
+        };
+
+        using data_type = std::array<word_t, word_count>;
+        data_type _data{};
+
+    public:
+
+        constexpr extended_word() = default;
+        constexpr explicit extended_word(word_t init_value) : _data{init_value, 0, 0, 0}
+        {}
+
+        constexpr bool operator[](std::ptrdiff_t const index) const noexcept {
+            assert(index < static_cast<std::ptrdiff_t>(size()));
+            auto [word_idx, word_offset] = to_local_index(index);
+            return std::bitset<bits_per_word>{_data[word_idx]}[word_offset];
+        }
+
+        constexpr extended_word & operator<<=(size_t const shift) noexcept {
+            assert(shift < bits_per_word);
+            word_t const shift_mask = (1 << shift) - 1;
+            word_t carry_bits{};
+            for (word_t & word : _data) {
+                word_t next_carry = std::rotl(word, shift) & shift_mask;
+                word = (word << shift) | carry_bits;
+                carry_bits = next_carry;
+            }
+            return *this;
+        }
+
+        constexpr extended_word & operator|=(word_t const & rhs) noexcept {
+            _data[0] |= rhs;
+            return *this;
+        }
+
+        constexpr size_t size() const noexcept {
+            return unpack_apply(_data, [] (auto ...words) {
+                return std::max({words.bit_width()...});
+            });
+        }
+
+        static constexpr size_t max_size() noexcept {
+            return bits_per_word * word_count;
+        }
+
+        template <seqan3::cereal_output_archive archive_t>
+        void save(archive_t const & oarchive) const
+        {
+            oarchive(_data);
+        }
+
+        template <seqan3::cereal_input_archive archive_t>
+        void load(archive_t &iarchive) noexcept
+        {
+            iarchive(_data);
+        }
+    private:
+
+        static constexpr std::pair<size_t, size_t> to_local_index(size_t const index) noexcept {
+            return {index >> 6, index & (bits_per_word - 1)};
+        }
+
+        template <typename data_t, typename fn_t>
+        static constexpr auto unpack_apply(data_t && data, fn_t && apply) noexcept {
+            return []
+            <typename _data_t, typename _fn_t, size_t ...idx>
+            (_data_t && data, _fn_t && apply, std::index_sequence<idx...> const &) constexpr {
+                return apply(indexed_word<idx, std::ranges::range_reference_t<_data_t>>{data[idx]}...);
+            } ((data_t &&)data, (fn_t &&)apply, std::make_index_sequence<word_count>());
+        }
+
+        friend constexpr bool operator==(extended_word const &, extended_word const &) noexcept = default;
+        friend constexpr std::strong_ordering operator<=>(extended_word const &, extended_word const &) noexcept = default;
+    };
     class alternate_path_descriptor {
     protected:
 
-        using word_type = uint_fast64_t;
-        using path_type = std::bitset<sizeof(word_type)>;
+        using data_type = extended_word<uint_fast64_t>;
 
         static constexpr bool ref_mask{0};
         static constexpr bool alt_mask{1};
@@ -36,11 +129,11 @@ namespace libjst
 
             friend alternate_path_descriptor;
 
-            path_type _path{};
+            data_type const * _path{};
             size_t _active_bit{};
 
-            explicit constexpr iterator(word_type word, size_t const active_bit) noexcept :
-                _path{std::move(word)},
+            explicit constexpr iterator(data_type const * word, size_t const active_bit) noexcept :
+                _path{word},
                 _active_bit{active_bit}
             {}
 
@@ -54,7 +147,8 @@ namespace libjst
             constexpr iterator() = default;
 
             constexpr reference operator*() const noexcept {
-                return _path[_active_bit - 1];
+                assert(_path != nullptr);
+                return (*_path)[_active_bit - 1];
             }
 
             constexpr reference operator[](difference_type const offset) const noexcept {
@@ -115,23 +209,23 @@ namespace libjst
             }
         };
 
-        word_type _word{1};
+        data_type _word{1};
 
     public:
 
         constexpr alternate_path_descriptor() = default;
 
         constexpr void next() noexcept {
-            assert((get_word() & 1ull << (max_size() - 1)) == 0);
+            assert(get_word().size() < get_word().max_size());
             get_word() <<= 1;
         }
 
         constexpr size_t size() const noexcept {
-            return std::bit_width(get_word());
+            return get_word().size();
         }
 
-        constexpr size_t max_size() const noexcept {
-            return sizeof(word_type) << 3;
+        static constexpr size_t max_size() noexcept {
+            return data_type::max_size();
         }
 
         constexpr void set_alt() noexcept {
@@ -143,11 +237,11 @@ namespace libjst
         }
 
         constexpr iterator begin() const noexcept {
-            return iterator{get_word(), size()};
+            return iterator{std::addressof(get_word()), size()};
         }
 
         constexpr iterator end() const noexcept {
-            return iterator{get_word(), 0};
+            return iterator{std::addressof(get_word()), 0};
         }
 
         template <seqan3::cereal_output_archive archive_t>
@@ -164,13 +258,36 @@ namespace libjst
 
     private:
 
-        constexpr word_type & get_word() noexcept {
+        constexpr data_type & get_word() noexcept {
             return _word;
         }
 
-        constexpr word_type const & get_word() const noexcept {
+        constexpr data_type const & get_word() const noexcept {
             return _word;
         }
+
+        friend constexpr bool operator==(alternate_path_descriptor const &,
+                                         alternate_path_descriptor const &) noexcept = default;
+
+        friend constexpr std::strong_ordering operator<=>(alternate_path_descriptor const &,
+                                                          alternate_path_descriptor const &) noexcept = default;
     };
+
+    template <typename char_t, typename char_traits_t, typename alt_path_descriptor_t>
+        requires std::same_as<std::remove_cvref_t<alt_path_descriptor_t>, alternate_path_descriptor>
+    inline std::basic_ostream<char_t, char_traits_t> & operator<<(std::basic_ostream<char_t, char_traits_t> & stream,
+                                                                  alt_path_descriptor_t && descriptor)
+    {
+        stream << "[";
+        if (descriptor.size() != 0) {
+            auto it = descriptor.begin();
+            for (auto it = descriptor.begin(); it != descriptor.end() - 1; ++it) {
+                stream << *it << ", ";
+            }
+            stream << *it;
+        }
+        stream << "]";
+        return stream;
+    }
 
 }  // namespace libjst
