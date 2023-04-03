55481c390a577505c00546dc32c561c3aae601b7
rrahn <rene.rahn@fu-berlin.de>
2023-04-03 17:23:44 +0200

    [LIB] Add classes to describe coverage information of an RCMS object.
    This is a separate abstraction layer providing the necessary interfaces to handle coverage operations on the breakpoints during the JST traversal.
    It includes a coverage based on a bitvector implementation and a coverage based on a std::set over integers.

---
+++ libjst/libjst/coverage/int_coverage.hpp
@@ -0,0 +1,151 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides int coverage.
+ * \author Rene Rahn <rene.rahn AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <algorithm>
+#include <ranges>
+
+#include <seqan3/core/concept/cereal.hpp>
+
+#include <libjst/coverage/range_domain.hpp>
+#include <libjst/utility/sorted_vector.hpp>
+
+namespace libjst
+{
+
+    template <std::unsigned_integral value_t>
+    class int_coverage {
+
+        using coverage_domain_t = range_domain<value_t>;
+        using domain_value_type = typename coverage_domain_t::value_type;
+        using data_type = sorted_vector<domain_value_type>;
+
+        data_type _data{};
+        [[no_unique_address]] coverage_domain_t _domain{};
+
+        // needs its own iterator and output iterator?
+
+    public:
+        using value_type = domain_value_type;
+        using iterator = std::ranges::iterator_t<data_type const>;
+
+        explicit constexpr int_coverage() = default;
+        explicit constexpr int_coverage(coverage_domain_t domain) noexcept :
+            _data{},
+            _domain{std::move(domain)}
+        {}
+
+        explicit constexpr int_coverage(std::initializer_list<value_type> init_list, coverage_domain_t domain) :
+            int_coverage{std::move(domain)}
+        {
+            std::ranges::for_each(init_list, [&] (value_type elem) {
+            if (!get_domain().is_member(elem))
+                throw std::domain_error{"The given element " + std::to_string(elem) + " is no member of the coverage domain!"};
+                _data.emplace_hint(_data.end(), std::move(elem));
+            });
+        }
+
+        constexpr iterator insert(value_type elem) {
+            if (!get_domain().is_member(elem))
+                throw std::domain_error{"The given element " + std::to_string(elem) + " is no member of the coverage domain!"};
+
+            return _data.insert(elem);
+        }
+
+        constexpr void clear() noexcept {
+            _data.clear();
+        }
+
+        constexpr iterator erase(iterator first) noexcept {
+            return erase(first, std::ranges::next(first));
+        }
+
+        constexpr iterator erase(iterator first, iterator last) noexcept {
+            return _data.erase(first, last);
+        }
+
+        constexpr value_type front() const noexcept {
+            assert(!empty());
+            return *begin();
+        }
+
+        constexpr value_type back() const noexcept {
+            assert(!empty());
+            return *std::ranges::prev(end());
+        }
+
+        constexpr bool empty() const noexcept {
+            return _data.empty();
+        }
+
+        constexpr size_t size() const noexcept {
+            return _data.size();
+        }
+
+        constexpr size_t max_size() const noexcept {
+            return get_domain().size();
+        }
+
+        constexpr coverage_domain_t const & get_domain() const noexcept {
+            return _domain;
+        }
+
+        constexpr iterator begin() const noexcept {
+            return _data.begin();
+        }
+
+        constexpr iterator end() const noexcept {
+            return _data.end();
+        }
+
+        // ----------------------------------------------------------------------------
+        // Serialisation
+        // ----------------------------------------------------------------------------
+
+        template <seqan3::cereal_input_archive archive_t>
+        void load(archive_t & iarchive)
+        {
+            iarchive(_data, _domain);
+        }
+
+        template <seqan3::cereal_output_archive archive_t>
+        void save(archive_t & oarchive) const
+        {
+            oarchive(_data, _domain);
+        }
+
+    private:
+
+        constexpr friend bool operator==(int_coverage const &, int_coverage const &) noexcept = default;
+
+        constexpr friend int_coverage
+        tag_invoke(std::tag_t<coverage_intersection>, int_coverage const & first, int_coverage const & second) {
+            if (first.get_domain() != second.get_domain())
+                throw std::domain_error{"Trying to intersect elements from different coverage domains."};
+
+            int_coverage result{};
+            std::ranges::set_intersection(first, second, std::back_inserter(result));
+            return result;
+        }
+
+        constexpr friend int_coverage
+        tag_invoke(std::tag_t<coverage_difference>, int_coverage const & first, int_coverage const & second) {
+            if (first.get_domain() != second.get_domain())
+                throw std::domain_error{"Trying to intersect elements from different coverage domains."};
+
+            int_coverage result{};
+            std::ranges::set_difference(first, second, std::back_inserter(result));
+            return result;
+        }
+    };
+}  // namespace libjst
