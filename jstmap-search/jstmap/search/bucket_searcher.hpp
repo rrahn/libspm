bdb547dab57b81b946eb3105080c4ae39d6869ad
rrahn <rene.rahn@fu-berlin.de>
2023-05-17 08:32:56 +0200

    [APP] Improve performance of search by skipping already visited locations in the RCMS.
    Using this heuristic, we can skip verifications if multiple seeds of the same query
    match with no errors at the same location in the RCMS.

--- jstmap-search/jstmap/search/bucket_searcher.hpp
+++ jstmap-search/jstmap/search/bucket_searcher.hpp
@@ -23,6 +23,7 @@ namespace jstmap
     private:
 
         bucket_t _bucket;
+        std::vector<std::ptrdiff_t> _last_position{};
         double _error_rate;
         /*parameters*/
     public:
@@ -31,19 +32,41 @@ namespace jstmap
         explicit bucket_searcher(bucket_t bucket, double error_rate) noexcept :
             _bucket{std::move(bucket)},
             _error_rate{error_rate}
-        {}
+        {
+            _last_position.resize(_bucket.needle_list.size(), -1);
+        }
 
         template <typename callback_t>
-        constexpr void operator()(callback_t && callback) const {
+        constexpr void operator()(callback_t && callback) {
             // instantiate pigeonhole filter.
             pigeonhole_filter filter{_bucket, _error_rate};
             // run pigeonhole filter on subtree
             // std::cout << "Run filter\n" << std::flush;
             filter([&] (auto && cargo, auto && finder, auto && needle_position) {
+                if (position_available(cargo, finder, needle_position)) return;
+
                 uint32_t seed_size = endPosition(finder) - beginPosition(finder);
                 seed_verifier verifier{_bucket, _error_rate, seed_size};
                 verifier(cargo, finder, needle_position, callback);
             });
+        }
+
+    private:
+
+        template <typename cargo_t, typename finder_t, typename needle_hit_t>
+        constexpr bool position_available(cargo_t const & cargo,
+                                          finder_t const & finder,
+                                          needle_hit_t const & hit) noexcept
+        {
+            std::ptrdiff_t label_offset = std::ranges::ssize(cargo.sequence()) - beginPosition(finder);
+            std::ptrdiff_t global_begin_pos = std::ranges::ssize(cargo.path_sequence()) - label_offset - hit.offset;
+            assert(global_begin_pos >= 0);
+
+            if (_last_position[hit.index] != global_begin_pos) {
+                _last_position[hit.index] = global_begin_pos;
+                return false;
+            }
+            return true;
         }
     };
 
