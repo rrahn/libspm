d3a3415ea727eda2a272984329bcd3e130f67c66
rrahn <rene.rahn@fu-berlin.de>
2023-05-22 16:38:47 +0200

    Disable parallel search for ibf based online pattern benchmarks

--- test/benchmark/online_pattern_ibf1024/shiftor_pattern_oblivious_chr22_needle32_ibf1024_benchmark.cpp
+++ test/benchmark/online_pattern_ibf1024/shiftor_pattern_oblivious_chr22_needle32_ibf1024_benchmark.cpp
@@ -20,7 +20,7 @@ BENCHMARK_TEMPLATE_DEFINE_F(fixture_oblivious_pattern_ibf, shiftor, capture<&chr
 }
 
 BENCHMARK_REGISTER_F(fixture_oblivious_pattern_ibf, shiftor)
-    ->RangeMultiplier(2)->Range(1,std::thread::hardware_concurrency())
+    ->RangeMultiplier(2)->Range(1,1)
     ->UseRealTime();
 
 } // namespace just::bench
