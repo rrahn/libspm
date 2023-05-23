61bbf693b736d676ad51f3fe070a2b8cfc3d087b
rrahn <rene.rahn@fu-berlin.de>
2023-05-23 20:10:57 +0200

    [BENCH] Add benchmarks for plain sequence analysis.

--- test/benchmark/online_pattern/myers_pattern_plain_benchmark.cpp
+++ test/benchmark/online_pattern/myers_pattern_plain_benchmark.cpp
@@ -14,7 +14,8 @@
 #include <jstmap/global/load_jst.hpp>
 #include <jstmap/search/load_queries.hpp>
 
-#include <seqan/find.h>
+#include <libjst/matcher/myers_matcher.hpp>
+#include <libjst/rcms/haplotype_viewer.hpp>
 
 template <typename rcs_store_t>
 inline size_t total_bytes(rcs_store_t const & rcs_store) noexcept {
@@ -22,62 +23,59 @@ inline size_t total_bytes(rcs_store_t const & rcs_store) noexcept {
 }
 
 template <typename ...args_t>
-static void naive_pattern(benchmark::State & state, args_t && ...args)
+static void myers_pattern(benchmark::State & state, args_t && ...args)
 {
     using sequence_t = jstmap::reference_t;
     auto [jst_file, needle_file] = std::tuple{args...};
     jstmap::rcs_store_t rcs_store = jstmap::load_jst(jst_file);
     sequence_t needle = jstmap::load_queries(needle_file)[0].sequence();
 
-    size_t sequence_count = rcs_store.size();
-    size_t batch_size = 16;
-    size_t total_runs = sequence_count / batch_size;
-    std::vector<sequence_t> batch{batch_size, sequence_t{rcs_store.source().begin(),rcs_store.source().end()}};
+    libjst::haplotype_viewer viewer{rcs_store};
 
-    seqan::Pattern<sequence_t, seqan::Simple> pattern{needle};
+    float const error_rate = static_cast<float>(state.range(0)) / 100.0 + 0.00001;
+    libjst::myers_matcher pattern(needle, std::ceil(needle.size() * error_rate));
     size_t hit_count{};
     for (auto _ : state)
     {
         hit_count = 0;
-        for (size_t run = 0; run < total_runs; ++run) {
-            std::ranges::for_each(batch, [&] (auto const & seq) {
-                using range_t = std::remove_reference_t<decltype(seq)>;
-                seqan::Finder<range_t> finder{seq};
-                while (seqan::find(finder, pattern)) {
-                    ++hit_count;
-                }
-            });
+        for (size_t idx = 0; idx < viewer.size(); ++idx) {
+            pattern(viewer[idx], [&] (auto const &) { ++hit_count; });
         }
+
     }
     state.counters["bytes"] = total_bytes(rcs_store);
     state.counters["bytes_per_second"] = seqan3::test::bytes_per_second(total_bytes(rcs_store));
 }
 
-BENCHMARK_CAPTURE(naive_pattern,
+BENCHMARK_CAPTURE(myers_pattern,
                   online_pattern_plain_needle32,
                   DATADIR"ALL.chr22.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.jst",
                   DATADIR"needle32.fa")
+                    ->DenseRange(3, 3, 1)
                     ->MeasureProcessCPUTime()
                     ->UseRealTime();
 
-BENCHMARK_CAPTURE(naive_pattern,
+BENCHMARK_CAPTURE(myers_pattern,
                   online_pattern_plain_needle64,
                   DATADIR"ALL.chr22.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.jst",
                   DATADIR"needle64.fa")
+                    ->DenseRange(3, 3, 1)
                     ->MeasureProcessCPUTime()
                     ->UseRealTime();
 
-BENCHMARK_CAPTURE(naive_pattern,
+BENCHMARK_CAPTURE(myers_pattern,
                   online_pattern_plain_needle128,
                   DATADIR"ALL.chr22.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.jst",
                   DATADIR"needle128.fa")
+                    ->DenseRange(3, 3, 1)
                     ->MeasureProcessCPUTime()
                     ->UseRealTime();
 
-BENCHMARK_CAPTURE(naive_pattern,
+BENCHMARK_CAPTURE(myers_pattern,
                   online_pattern_plain_needle256,
                   DATADIR"ALL.chr22.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.jst",
                   DATADIR"needle256.fa")
+                    ->DenseRange(3, 3, 1)
                     ->MeasureProcessCPUTime()
                     ->UseRealTime();
 
