f7a4cd39aa6fddbc6b5ae5f38e2c6c64057b18b0
rrahn <rene.rahn@fu-berlin.de>
2023-09-20 12:27:52 +0200

    [TEST] Use catch2 in journal entry test instead of google test.

--- test/api/libjst/sequence/journal_entry_test.cpp
+++ test/api/libjst/sequence/journal_entry_test.cpp
@@ -5,126 +5,99 @@
 // shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
 // -----------------------------------------------------------------------------------------------------
 
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
 
 #include <vector>
 
 #include <libjst/sequence/journal_entry.hpp>
 
-// Scenario: Initialization of journal entry
-// Given a default initialized journal entry
-// When accessing the begin position
-// Then the begin position is 0
-// And the segment is empty
-// And the end position is 0
-TEST(journal_entry_test, default_constructor)
+SCENARIO("Journal entry can be default initialized", "[sequence][journal_entry]")
 {
-    using journal_entry_t = libjst::journal_entry<size_t, std::vector<char>::iterator>;
-    EXPECT_TRUE((std::default_initializable<journal_entry_t>));
-    journal_entry_t entry{};
-    EXPECT_EQ(entry.begin_position(), 0u);
-    EXPECT_EQ(entry.end_position(), 0u);
-    EXPECT_TRUE(entry.segment().empty());
+    GIVEN("A default initialized journal entry")
+    {
+        using journal_entry_t = libjst::journal_entry<size_t, std::vector<char>::iterator>;
+        journal_entry_t entry{};
+        THEN("the begin position is 0")
+        {
+            REQUIRE(entry.begin_position() == 0u);
+        }
+        AND_THEN("the segment is empty")
+        {
+            REQUIRE(entry.segment().empty());
+        }
+        AND_THEN("the end position is 0")
+        {
+            REQUIRE(entry.end_position() == 0u);
+        }
+    }
 }
 
-// Given a journal entry initialized with a position and a sequence segment
-// When accessing the begin position
-// Then the begin position is returned
-TEST(journal_entry_test, begin_position)
+SCENARIO("Journal entry can be initialized with a position and a sequence", "[sequence],[journal_entry]")
+{
+    GIVEN("A journal entry initialized with a position and a sequence segment")
+    {
+        std::vector sequence{'A', 'C', 'G', 'T'};
+        libjst::journal_entry entry{42u, sequence};
+        THEN("the begin position is the given position")
+        {
+            REQUIRE(entry.begin_position() == 42u);
+        }
+        AND_THEN("the segment is the given sequence")
+        {
+            REQUIRE(entry.segment().data() == sequence.data());
+        }
+        AND_THEN("the end position is the given position + the size of the sequence")
+        {
+            REQUIRE(entry.end_position() == 46u);
+        }
+    }
+}
+
+SCENARIO("Two journal entries can be compared", "[sequence],[journal_entry]")
 {
     std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry{42u, sequence};
-    EXPECT_EQ(entry.begin_position(), 42u);
-}
-
-// Given a journal entry initialized with a position and a sequence segment
-// When accessing the end position
-// Then the begin position + the size of the segment is returned
-TEST(journal_entry_test, end_position)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry{42u, sequence};
-    EXPECT_EQ(entry.end_position(), 46u);
-}
-
-// When accessing the segment
-// Then the segment is returned
-TEST(journal_entry_test, segment)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry{42u, sequence};
-    EXPECT_EQ(entry.segment().data(), sequence.data());
-}
-
-// Scenario: Compare two journal entries
-// Given two initialized journal entries
-// When both journal entries have the same begin position and the same segment
-// Then the journal entries are equivalent
-TEST(journal_entry_test, equality)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry1{42u, sequence};
-    libjst::journal_entry entry2{42u, sequence};
-    EXPECT_EQ(entry1 <=> entry2, std::weak_ordering::equivalent);
-    EXPECT_EQ(entry1, entry2);
-}
-
-// Given two initialized journal entries
-// When both journal entries have different begin positions but the same segment
-// Then the journal entries are not equivalent
-TEST(journal_entry_test, inequality_begin_position)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry1{42u, sequence};
-    libjst::journal_entry entry2{43u, sequence};
-    EXPECT_NE(entry1, entry2);
-}
-
-// Given two initialized journal entries
-// When both journal entries hold different segments but the same begin position
-// Then the journal entries are equivalent
-TEST(journal_entry_test, inequality_segment)
-{
-    std::vector sequence1{'A', 'C', 'G', 'T'};
-    std::vector sequence2{'A', 'C', 'G', 'T', 'A'};
-    libjst::journal_entry entry1{42u, sequence1};
-    libjst::journal_entry entry2{42u, sequence2};
-    EXPECT_EQ(entry1, entry2);
-}
-
-// Given two initialized journal entries
-// When both journal entries hold different segments and different begin positions
-// Then the journal entries are not equivalent
-TEST(journal_entry_test, inequality_segment_and_begin_position)
-{
-    std::vector sequence1{'A', 'C', 'G', 'T'};
-    std::vector sequence2{'A', 'C', 'G', 'T', 'A'};
-    libjst::journal_entry entry1{42u, sequence1};
-    libjst::journal_entry entry2{43u, sequence2};
-    EXPECT_NE(entry1, entry2);
-}
-
-// Scenario: Compare ordering of two journal entries
-// Given two initialized journal entries
-// When the begin position of the first journal entry is less than the begin position of the second journal entry
-// Then the first journal entry is less than the second journal entry
-TEST(journal_entry_test, less_than)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry1{42u, sequence};
-    libjst::journal_entry entry2{43u, sequence};
-    EXPECT_EQ(entry1 <=> entry2, std::weak_ordering::less);
-    EXPECT_LT(entry1, entry2);
-}
-
-// Given two initialized journal entries
-// When the begin position of the first journal entry is greater than the begin position of the second journal entry
-// Then the first journal entry is greater than the second journal entry
-TEST(journal_entry_test, greater_than)
-{
-    std::vector sequence{'A', 'C', 'G', 'T'};
-    libjst::journal_entry entry1{43u, sequence};
-    libjst::journal_entry entry2{42u, sequence};
-    EXPECT_EQ(entry1 <=> entry2, std::weak_ordering::greater);
-    EXPECT_GT(entry1, entry2);
+    GIVEN("Two journal entries with the same begin position and the same segment")
+    {
+        libjst::journal_entry entry1{42u, sequence};
+        libjst::journal_entry entry2{42u, sequence};
+        THEN("the journal entries are equivalent")
+        {
+            REQUIRE(entry1 <=> entry2 == std::weak_ordering::equivalent);
+            REQUIRE(entry1 == entry2);
+        }
+    }
+    AND_GIVEN("Two journal entries with the same begin position but different segments")
+    {
+        std::vector sequence2{'A', 'C', 'G', 'T'};
+        libjst::journal_entry entry1{42u, sequence};
+        libjst::journal_entry entry2{42u, sequence2};
+        THEN("the journal entries are not equivalent")
+        {
+            REQUIRE(entry1 != entry2);
+        }
+    }
+    AND_GIVEN("Two journal entries with different begin positions but the same segment")
+    {
+        libjst::journal_entry entry1{42u, sequence};
+        libjst::journal_entry entry2{43u, sequence};
+        THEN("the journal entries are not equivalent")
+        {
+            REQUIRE(entry1 != entry2);
+        }
+    }
+    AND_GIVEN("A journal entry with lower begin position than the second journal entry")
+    {
+        libjst::journal_entry entry1{42u, sequence};
+        libjst::journal_entry entry2{43u, sequence};
+        THEN("the first journal entry is less than the second journal entry")
+        {
+            REQUIRE(entry1 <=> entry2 == std::weak_ordering::less);
+            REQUIRE(entry1 < entry2);
+        }
+        AND_THEN("the second journal entry is greater than the first journal entry")
+        {
+            REQUIRE(entry2 <=> entry1 == std::weak_ordering::greater);
+            REQUIRE(entry2 > entry1);
+        }
+    }
 }
