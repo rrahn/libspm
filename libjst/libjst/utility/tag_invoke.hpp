085b9a60cdccfde61ddb3191c44bc081726eae56
rrahn <rene.rahn@fu-berlin.de>
2023-10-24 11:55:57 +0200

    [LIB] Extend tag_invoke by a wrapper to also check for available member function to overload a CPO.

--- libjst/libjst/utility/tag_invoke.hpp
+++ libjst/libjst/utility/tag_invoke.hpp
@@ -13,6 +13,8 @@
 
 #pragma once
 
+#include <concepts>
+#include <functional>
 #include <type_traits>
 #include <utility>
 
@@ -28,37 +30,6 @@ namespace libjst
 
         // Poison pill to prohibit ADL for tag_invoke further up in the namespace stack.
         void tag_invoke();
-
-        template <typename tag_t, typename... args_t>
-        concept tag_invocable = requires(tag_t &&tag, args_t &&...args) {
-            tag_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...);
-        };
-
-        template <typename tag_t, typename... args_t>
-        concept nothrow_tag_invocable = tag_invocable<tag_t, args_t...> && requires(tag_t &&tag, args_t &&...args) {
-            {
-                tag_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...)
-            } noexcept;
-        };
-
-        template <typename tag_t, typename... args_t>
-        inline constexpr bool is_nothrow_tag_invocable_v = false;
-
-        template <typename tag_t, typename... args_t>
-            requires nothrow_tag_invocable<tag_t, args_t...>
-        inline constexpr bool is_nothrow_tag_invocable_v<tag_t, args_t...> = true;
-
-        template <typename tag_t, typename... args_t>
-        using tag_invoke_result_t = decltype(tag_invoke(std::declval<tag_t>(), std::declval<args_t>()...));
-
-        template <typename tag_t, typename... args_t>
-        struct tag_invoke_result
-        {
-            using type = tag_invoke_result_t<tag_t, args_t...>; // present if and only if tag_invocable<tag_t, args_t...> is true
-        };
-
-        template <auto &tag_v>
-        using tag_t = std::remove_cvref_t<decltype(tag_v)>;
 
         struct fn
         {
@@ -76,6 +47,37 @@ namespace libjst
             inline constexpr _tag_invoke::fn tag_invoke{};
         } // namespace _cpo
 
+        template <typename tag_t, typename... args_t>
+        concept tag_invocable = requires(tag_t &&tag, args_t &&...args) {
+            _cpo::tag_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...);
+        };
+
+        template <typename tag_t, typename... args_t>
+        concept nothrow_tag_invocable = tag_invocable<tag_t, args_t...> && requires(tag_t &&tag, args_t &&...args) {
+            {
+                _cpo::tag_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...)
+            } noexcept;
+        };
+
+        template <typename tag_t, typename... args_t>
+        inline constexpr bool is_nothrow_tag_invocable_v = false;
+
+        template <typename tag_t, typename... args_t>
+            requires nothrow_tag_invocable<tag_t, args_t...>
+        inline constexpr bool is_nothrow_tag_invocable_v<tag_t, args_t...> = true;
+
+        template <typename tag_t, typename... args_t>
+        using tag_invoke_result_t = decltype(_cpo::tag_invoke(std::declval<tag_t>(), std::declval<args_t>()...));
+
+        template <typename tag_t, typename... args_t>
+        struct tag_invoke_result
+        {
+            using type = tag_invoke_result_t<tag_t, args_t...>; // present if and only if tag_invocable<tag_t, args_t...> is true
+        };
+
+        template <auto &tag_v>
+        using tag_t = std::remove_cvref_t<decltype(tag_v)>;
+
     } // namespace _tag_invoke
     /// @endcond
 
@@ -86,5 +88,116 @@ namespace libjst
     using _tag_invoke::tag_invoke_result;
     using _tag_invoke::tag_invoke_result_t;
     using _tag_invoke::tag_t;
+
+    namespace _get_tag_member {
+
+        inline constexpr struct fn {
+            template <typename tag_t, typename ...args_t>
+                requires tag_invocable<fn, tag_t, args_t...>
+            constexpr auto operator()(tag_t && tag, args_t && ...args) const
+                noexcept(nothrow_tag_invocable<fn, tag_t, args_t...>)
+                -> tag_invoke_result_t<fn, tag_t, args_t...>
+            {
+                return tag_invoke(fn{}, (tag_t&&)tag, (args_t &&)args...);
+            }
+        } get_tag_member;
+
+        template <typename tag_t, typename ...args_t>
+        concept get_tag_member_invocable = tag_invocable<_get_tag_member::fn, tag_t, args_t...>;
+
+        template <typename tag_t, typename ...args_t>
+        concept nothrow_get_tag_member_invocable = nothrow_tag_invocable<_get_tag_member::fn, tag_t, args_t...>;
+
+        template <typename tag_t, typename ...args_t>
+            requires get_tag_member_invocable<tag_t, args_t...>
+        using get_tag_member_t = tag_invoke_result_t<_get_tag_member::fn, tag_t, args_t...>;
+    }
+
+    using _get_tag_member::get_tag_member;
+    using _get_tag_member::get_tag_member_invocable;
+    using _get_tag_member::nothrow_get_tag_member_invocable;
+    using _get_tag_member::get_tag_member_t;
+
+    namespace _member_invoke
+    {
+        inline constexpr struct fn {
+            template <typename tag_t, typename ...args_t>
+                requires get_tag_member_invocable<tag_t, args_t...>
+            constexpr auto operator()(tag_t && tag, args_t && ...args) const
+                noexcept(nothrow_get_tag_member_invocable<tag_t, args_t...> &&
+                         std::is_nothrow_invocable_v<get_tag_member_t<tag_t, args_t...>, args_t...>)
+                -> std::invoke_result_t<get_tag_member_t<tag_t, args_t...>, args_t...>
+            {
+                return std::invoke(get_tag_member((tag_t&&)tag, (args_t &&)args...),
+                                   (args_t &&)args...);
+            }
+        } member_invoke;
+
+        template <typename tag_t, typename ...args_t>
+        concept member_invocable = requires (tag_t && tag, args_t && ...args)
+        {
+            { member_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...) };
+        };
+
+        template <typename tag_t, typename ...args_t>
+        concept nothrow_member_invocable = requires (tag_t && tag, args_t && ...args)
+        {
+            { member_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...) } noexcept;
+        };
+
+        template <typename tag_t, typename ...args_t>
+            requires member_invocable<tag_t, args_t...>
+        using member_invoke_result_t = decltype(member_invoke(std::declval<tag_t &&>(), std::declval<args_t &&>()...));
+    }
+
+    using _member_invoke::member_invoke;
+    using _member_invoke::member_invocable;
+    using _member_invoke::nothrow_member_invocable;
+    using _member_invoke::member_invoke_result_t;
+
+    namespace _tag_or_member_invoke {
+
+        inline constexpr struct fn {
+
+            template <typename tag_t, typename... args_t>
+                requires tag_invocable<tag_t, args_t...>
+            constexpr auto operator()(tag_t &&tag, args_t &&...args) const
+                noexcept(nothrow_tag_invocable<tag_t, args_t...>)
+                -> tag_invoke_result_t<tag_t, args_t...>
+            {
+                return tag_invoke(std::forward<tag_t>(tag), std::forward<args_t>(args)...);
+            }
+
+            template <typename tag_t, typename... args_t>
+                requires (!tag_invocable<tag_t, args_t...>) && member_invocable<tag_t, args_t...>
+            constexpr auto operator()(tag_t &&tag, args_t &&...args) const
+                noexcept(nothrow_member_invocable<tag_t, args_t...>)
+                -> member_invoke_result_t<tag_t, args_t...>
+            {
+                return member_invoke((tag_t &&)tag, (args_t &&)args...);
+            }
+        } tag_or_member_invoke;
+
+        template <typename tag_t, typename ...args_t>
+        concept tag_or_member_invocable = requires (tag_t && tag, args_t && ...args)
+        {
+            { tag_or_member_invoke((tag_t &&) tag, (args_t &&)args...) };
+        };
+
+        template <typename tag_t, typename ...args_t>
+        concept nothrow_tag_or_member_invocable = requires (tag_t && tag, args_t && ...args)
+        {
+            { tag_or_member_invoke((tag_t &&) tag, (args_t &&)args...) } noexcept;
+        };
+
+        template <typename tag_t, typename ...args_t>
+            requires tag_or_member_invocable<tag_t, args_t...>
+        using tag_or_member_invoke_result_t = decltype(tag_or_member_invoke(std::declval<tag_t &&>(), std::declval<args_t&&>()...));
+    }
+
+    using _tag_or_member_invoke::tag_or_member_invoke;
+    using _tag_or_member_invoke::tag_or_member_invocable;
+    using _tag_or_member_invoke::nothrow_tag_or_member_invocable;
+    using _tag_or_member_invoke::tag_or_member_invoke_result_t;
 
 } // namespace libjst
